4544db18ded8cb1c28a9f961e6389b8e306462f2
suleymandanalioglu <suleyman.danalioglu@arabam.com>
2019-11-19 14:38:28 +0300

    fix indent

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,15 +2,15 @@ import './popup.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const button = document.getElementById('buttonGetPosts');
+const button = document.getElementById('buttonGetPosts');
 
-  button.addEventListener('click',  () => {
-    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-      chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.opacity = "0.5"'}
-      );
-    });
-  })
+button.addEventListener('click',  () => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    chrome.tabs.executeScript(
+      tabs[0].id,
+      {code: 'document.body.style.opacity = "0.5"'}
+    );
+  });
+})
 
 });
