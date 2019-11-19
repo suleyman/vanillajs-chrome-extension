b6f66291b7ec91ca2dc5ee48100066b32a22d788
suleymandanalioglu <suleyman.danalioglu@arabam.com>
2019-11-19 14:41:01 +0300

    updated button click function

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -5,12 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const button = document.getElementById('buttonGetPosts');
 
 button.addEventListener('click',  () => {
-  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      {code: 'document.body.style.opacity = "0.5"'}
-    );
-  });
+  alert('Button clicked!')
 })
 
 });
